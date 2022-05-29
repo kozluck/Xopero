e8867e6206b288d407d6a9599f9839814e68a682
kozluck <kozluck@icloud.com>
2022-05-29 23:22:44 +0200

    Basic version of DiffViewer

---
+++ diffViewer/diffViewer/DiffWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+
+namespace diffViewer
+{
+
+    public partial class DiffWindow : Window
+    {
+
+        public DiffWindow(string firstFileName, string secondFileName)
+        {
+            InitializeComponent();
+
+            var firstFileText = System.IO.File.ReadAllLines(firstFileName).ToList();
+            var secondFileText = System.IO.File.ReadAllLines(secondFileName).ToList();
+
+            var inFirstFileOnly = firstFileText.Except(secondFileText).ToList();
+            var inSecondFileOnly = secondFileText.Except(firstFileText).ToList();
+
+
+            List<string> fixedInFirstFileOnly = new List<string>();
+            List<string> fixedInSecondFileOnly = new List<string>();
+
+            foreach (var str in inFirstFileOnly)
+                fixedInFirstFileOnly.Add(str.Replace("\t", ""));
+
+            foreach (var str in inSecondFileOnly)
+                fixedInSecondFileOnly.Add(str.Replace("\t", ""));
+
+            firstFileTextBlock.Text = string.Join("\n", fixedInFirstFileOnly);
+            secondFileTextBlock.Text = string.Join("\n", fixedInSecondFileOnly);
+
+        }
+    }
+}
---
+++ diffViewer/diffViewer/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+
+
+namespace diffViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private string firstFileName, secondFileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void firstFile_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            var result = ofd.ShowDialog();
+            firstFile.Content = ofd.FileName;
+            firstFileName = ofd.FileName;
+
+        }
+
+        private void secondFile_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new Microsoft.Win32.OpenFileDialog();
+            var result = ofd.ShowDialog();
+            secondFile.Content = ofd.FileName;
+            secondFileName = ofd.FileName;
+        }
+
+        private void checkDifferences_Click(object sender, RoutedEventArgs e)
+        {
+            if (firstFileName != null && secondFileName != null)
+            {
+                this.Hide();
+                Window diffWindow = new DiffWindow(firstFileName, secondFileName);
+                diffWindow.Show();
+            }
+            else
+            {
+                MessageBox.Show("Choose 2 files to check!", "Error");
+            }
+        }
+    }
+}
